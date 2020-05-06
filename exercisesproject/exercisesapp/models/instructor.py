2ad1c4fb0be17cfc0c1204fddc5bdb9eb1fa1494
Kristen Norris <krnorris65@gmail.com>
2020-05-06 09:52:51 -0500

    an instructor is created when a new user is created

--- exercisesproject/exercisesapp/models/instructor.py
+++ exercisesproject/exercisesapp/models/instructor.py
@@ -7,16 +7,16 @@ from django.dispatch import receiver
 class Instructor(models.Model):
 
     user = models.OneToOneField(User, on_delete=models.CASCADE)
-    slack_handle = models.CharField(max_length=50)
-    specialty = models.CharField(max_length=50)
-    cohort = models.ForeignKey(Cohort, on_delete=models.CASCADE)
+    slack_handle = models.CharField(max_length=50, null=True)
+    specialty = models.CharField(max_length=50, null=True)
+    cohort = models.ForeignKey(Cohort, null=True, blank=True,on_delete=models.CASCADE)
 
 
-    # @receiver(post_save, sender=User)
-    # def create_instructor(sender, instance, created, **kwargs):
-    #     if created:
-    #         Instructor.objects.create(user=instance)
+    @receiver(post_save, sender=User)
+    def create_instructor(sender, instance, created, **kwargs):
+        if created:
+            Instructor.objects.create(user=instance)
 
-    # @receiver(post_save, sender=User)
-    # def save_instructor(sender, instance, **kwargs):
-    #     instance.instructor.save()
+    @receiver(post_save, sender=User)
+    def save_instructor(sender, instance, **kwargs):
+        instance.instructor.save()
